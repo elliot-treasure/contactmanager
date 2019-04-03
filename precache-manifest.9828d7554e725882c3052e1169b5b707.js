087aff3ad1fdf423a41a7bc4f751e5458b1bcf5d
Elliot Treasure <elliot.treasure@gmail.com>
2019-04-03 12:34:32 -0500

    Updates

---
+++ precache-manifest.9828d7554e725882c3052e1169b5b707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "4a9c7a188c44c092809f",
+    "url": "/contactmanager/static/js/main.12221f38.chunk.js"
+  },
+  {
+    "revision": "b3ba485d63cfff869272",
+    "url": "/contactmanager/static/js/2.8968a7eb.chunk.js"
+  },
+  {
+    "revision": "4a9c7a188c44c092809f",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "b3ba485d63cfff869272",
+    "url": "/contactmanager/static/css/2.f23ef35e.chunk.css"
+  },
+  {
+    "revision": "81900c087b2a3fe8da0ee4acf16c61c4",
+    "url": "/contactmanager/index.html"
+  }
+];
